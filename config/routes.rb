843c1949dc3772f6b1ac2416f448d02acb4eab7a
n-hata91 <htnoms5a@gmail.com>
2020-06-08 20:45:20 +0900

    add function delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'post_images#index'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy] #ネスト、URLがパンくずリストみたいになる
     resource :favorites, only: [:create, :destroy]
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
