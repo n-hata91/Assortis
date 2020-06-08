fdaecb0661af6efd6d666bfa07c7f33939454784
n-hata91 <htnoms5a@gmail.com>
2020-06-08 18:25:24 +0900

    add function like

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'post_images#index'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show]
+    resources :post_comments, only: [:create, :destroy] #ネスト、URLがパンくずリストみたいになる
+    resource :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
